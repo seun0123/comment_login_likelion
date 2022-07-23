b4b105086f645b0468af4a505a2f13abf60a7d8e
seun0123 <seunpark04@gmail.com>
2022-07-23 16:18:29 +0900

    first commit

---
+++ myapp/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Post, Comment, Hashtag
+
+class BlogForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'body', 'image', 'hashtags']
+
+class CommentForm(forms.ModelForm) :
+    class Meta :
+        model = Comment
+        fields = ['text']
+
+class HashtagForm(forms.ModelForm) :
+    class Meta :
+        model = Hashtag
+        fields = ['name']
